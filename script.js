a230cabce4b48838b225cf8cf30341b8771f6966
saciinol <darrenicolas18@gmail.com>
2024-11-26 13:34:37 +0800

    banner

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const headerNav = document.querySelector(".header__nav");
 burgir.addEventListener("click",  () => {
     burgir.classList.toggle("open");//turns the burgir to x
     headerNav.classList.toggle("open");//header nav appears
-});
+})
